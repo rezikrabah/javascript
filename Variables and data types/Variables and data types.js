1c66c528c60e8d01371f45bb0c027a26bf969ac9
rezik rabah <163780921+rezikrabah@users.noreply.github.com>
2024-08-13 00:48:30 +0100

    Add files via upload

---
+++ Variables and data types/Variables and data types.js
@@ -0,0 +1,27 @@
+let age; //number
+age=21;
+age=age+1;
+console.log("you are ",age,"y old");
+
+
+let number="29";
+age=number+1;
+console.log(age);
+
+
+
+
+let firstname="rabah"; //stings
+console.log( "hello",firstname);
+
+
+
+
+
+let student=true; //booleans
+console.log(student);
+document.getElementById("p1").innerHTML = "HELLO " + firstname ;
+document.getElementById("p2").innerHTML = "you are " + age + " years old";
+
+
+
---
+++ first/first.js
@@ -0,0 +1,17 @@
+console.log("i like pizza");
+console.log("its really good!");
+window.alert("i really love pizza");
+// this is a comment
+
+
+
+/*
+this
+is
+a
+multiline
+comment
+
+
+
+*/
