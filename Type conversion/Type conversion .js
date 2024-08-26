c251c4eeea01fd80b594660b6e60ec75477b137d
rezik rabah <163780921+rezikrabah@users.noreply.github.com>
2024-08-26 19:28:27 +0100

    Add files via upload

---
+++ Type conversion/Type conversion .js
@@ -0,0 +1,30 @@
+let age = window.prompt("how old are you");
+age = Number(age);
+age+=1;
+console.log(age , typeof age);
+
+
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+console.log(x , typeof x);
+console.log(y , typeof y);
+console.log(z, typeof z);
+
+
+
+
+
+let t = "0";
+let r = "0";
+let e = "0";
+t = Number(t);
+r = String(r);
+e = Boolean(e);
+console.log(t , typeof t);
+console.log(r , typeof r);
+console.log(e, typeof e);
